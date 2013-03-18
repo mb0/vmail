0ce07f9b834ae99fe122ab4ee010b12fd0044d1f
Martin Schnabel <mb0@mb0.org>
2013-03-18 23:06:49 +0100

    adds sqlite data store for virtual mail destinations.

---
+++ store/store.go
@@ -0,0 +1,87 @@
+// Copyright 2013 Martin Schnabel. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package store
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+const (
+	_ = iota
+	TypeBox
+	TypeAlias
+)
+
+type Dest struct {
+	Id     uint64
+	Type   int
+	Name   string
+	Domain string
+	Enable bool
+	Passwd string
+	Forwrd string
+}
+
+var CreateSql = `create table dest (
+	id integer primary key autoincrement,
+	type integer,
+	name text,
+	domain text,
+	enable integer,
+	passwd text,
+	forwrd text
+)`
+
+var DomainsSql = `select
+	distinct domain
+	from dest %s order by domain
+`
+
+var DestsSql = `select
+	id, type, name, domain, enable, passwd, forwrd
+	from dest %s order by domain, name
+`
+
+func Create(db *sql.DB) error {
+	_, err := db.Exec(CreateSql)
+	return err
+}
+
+func Domains(db *sql.DB, where string, args ...interface{}) ([]string, error) {
+	rows, err := db.Query(fmt.Sprintf(DomainsSql, where), args...)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var res []string
+	for rows.Next() {
+		var domain string
+		err = rows.Scan(&domain)
+		if err != nil {
+			return nil, err
+		}
+		res = append(res, domain)
+	}
+	return res, rows.Err()
+}
+
+func Dests(db *sql.DB, where string, args ...interface{}) ([]Dest, error) {
+	rows, err := db.Query(fmt.Sprintf(DestsSql, where), args...)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var res []Dest
+	for rows.Next() {
+		var dest Dest
+		err = rows.Scan(&dest.Id, &dest.Type, &dest.Name, &dest.Domain, &dest.Enable, &dest.Passwd, &dest.Forwrd)
+		if err != nil {
+			return nil, err
+		}
+		res = append(res, dest)
+	}
+	return res, rows.Err()
+}
---
+++ store/store_test.go
@@ -0,0 +1,56 @@
+// Copyright 2013 Martin Schnabel. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package store
+
+import (
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
+	"testing"
+)
+
+var FixtureSql = `insert into dest
+	(id, type, name, domain, enable, passwd, forwrd) values
+	(1, 1, 'mbnull', 'mbnull.org', 1, 'xxx', ''),
+	(2, 2, 'mb0', 'mb0.org', 1, '', 'mbnull@mbnull.org'),
+	(3, 2, 'mb0', 'mbnull.org', 0, '', 'mbnull@mbnull.org')
+`
+
+func TestStore(t *testing.T) {
+	db, err := sql.Open("sqlite3", ":memory:")
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = Create(db)
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = db.Exec(FixtureSql)
+	if err != nil {
+		t.Fatal(err)
+	}
+	doms, err := Domains(db, "where enable=1")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(doms) != 2 || doms[0] != "mb0.org" || doms[1] != "mbnull.org" {
+		t.Logf("got unexpected domains %v\n", doms)
+	}
+	boxes, err := Dests(db, "where enable=1 and type=?", TypeBox)
+	if err != nil {
+		t.Fatal(err)
+	}
+	box1 := Dest{1, TypeBox, "mbnull", "mbnull.org", true, "xxx", ""}
+	if len(boxes) != 1 || boxes[0] != box1 {
+		t.Logf("expect %v got %v\n", box1, boxes)
+	}
+	aliases, err := Dests(db, "where enable=1 and type=?", TypeAlias)
+	if err != nil {
+		t.Fatal(err)
+	}
+	alias1 := Dest{2, TypeAlias, "mb0", "mb0.org", true, "", "mbnull@mbnull.org"}
+	if len(aliases) != 1 || boxes[0] != alias1 {
+		t.Logf("expect alias %v got %v\n", alias1, aliases)
+	}
+}
